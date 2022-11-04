78a1f32b65eb7fa823ed2788bdf3fc3c3880e8fd
Tomas <tomibudeguer20@gmail.com>
2022-11-04 13:26:34 -0300

    agregue el archivo vercel y cambie la url

--- src/database.js
+++ src/database.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://localhost:27017/blog-recetas'
+// const url = 'mongodb://localhost:27017/blog-recetas' //bd local
+const url =
+  "mongodb+srv://TomasBudeguer:KJI5kQLnLz619pLl@cluster0.vjacqdu.mongodb.net/blog-recetas";
 
-mongoose.connect(url)
+mongoose.connect(url);
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
-connection.once('open', ()=>{
-    console.log('BD conectada')
-})
+connection.once("open", () => {
+  console.log("BD conectada");
+});
